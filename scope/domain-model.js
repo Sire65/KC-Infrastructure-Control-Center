c7f07eb9e006135ca8f9fa221bfec74f7409cada
Sire65 <ha-joko@web.de>
2026-08-26 08:42:54 +0200

    dev31 add NON_KC monitoring domain

--- scope/domain-model.js
+++ scope/domain-model.js
@@ -1,4 +1,4 @@
-export const DOMAIN={KC:'KC',PRIVATE:'PRIVATE'};
+export const DOMAIN={KC:'KC',PRIVATE:'PRIVATE',NON_KC:'NON_KC'};
 
 export function markDomain(resource,domain=DOMAIN.KC){
   resource.domain=domain;
@@ -7,10 +7,12 @@ export function markDomain(resource,domain=DOMAIN.KC){
 
 export function isKC(resource){return (resource?.domain||DOMAIN.KC)===DOMAIN.KC;}
 export function isPrivate(resource){return resource?.domain===DOMAIN.PRIVATE;}
+export function isNonKC(resource){return resource?.domain===DOMAIN.NON_KC;}
 
 export function scopeSummary(resources=[]){
   return {
     kc:resources.filter(isKC).length,
-    private:resources.filter(isPrivate).length
+    private:resources.filter(isPrivate).length,
+    nonKc:resources.filter(isNonKC).length
   };
 }
