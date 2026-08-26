4eba149d56eff3689e0bbd0197c5dba834915df3
Sire65 <ha-joko@web.de>
2026-08-26 06:08:00 +0200

    chore: include failover runtime in dev.7 cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kicc-0.1.0-dev.6';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./adapters/adapter-core.js','./adapters/github-repository-adapter.js','./adapters/indexeddb-local-adapter.js','./adapters/telemetry-bridge-adapter.js','./database/database-model.js'];
+const CACHE='kicc-0.1.0-dev.7';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./adapters/adapter-core.js','./adapters/github-repository-adapter.js','./adapters/indexeddb-local-adapter.js','./adapters/telemetry-bridge-adapter.js','./database/database-model.js','./failover/failover-state-machine.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{
