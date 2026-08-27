c0fc67505330ddff631556f9c17b412990f68d76
Sire65 <ha-joko@web.de>
2026-08-27 20:25:00 +0200

    Load morning mirror report on KICC startup

--- navigation/leitstand-tabs.js
+++ navigation/leitstand-tabs.js
@@ -16,6 +16,7 @@ import '../programs/remote-heartbeat-bridge-runtime.js';
 import '../programs/remote-heartbeat-bridge-ui.js';
 import '../telemetry/neon-runtime-config.js';
 import '../sync/mirror-runtime-config.js';
+import '../sync/mirror-morning-report.js';
 import '../products/program-version-lifecycle.js';
 import '../products/github-telemetry-runtime.js';
 import '../storage/recovery-policy.js';
