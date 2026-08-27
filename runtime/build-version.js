874fa567b0518cad4e8d1876b5d80dbb6795656e
Sire65 <ha-joko@web.de>
2026-08-27 09:05:30 +0200

    build: publish runtime version dev.79

---
+++ runtime/build-version.js
@@ -0,0 +1,6 @@
+const VERSION='0.1.0-dev.79';
+globalThis.KICC_BUILD_VERSION=VERSION;
+queueMicrotask(()=>{
+  if(globalThis.KICC)globalThis.KICC.version=VERSION;
+  const el=document.getElementById('version');if(el)el.textContent=VERSION;
+});
